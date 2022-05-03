702627dfcda1195fe4ca29a279e8c68b4cf8d67f
Vishwam Shriram Mundada <58394139+Vishwam36@users.noreply.github.com>
2022-05-03 11:47:52 +0530

    Easy

---
+++ Introductory Problems/Increasing Array.cpp
@@ -0,0 +1,25 @@
+// https://cses.fi/problemset/task/1094
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    for(int i = 0; i < n; ++i)
+        cin >> a[i];
+    
+    ll ans = 0;
+    for(int i = 1; i < n; ++i)
+        if(a[i] < a[i-1])
+        {
+            ans += (a[i-1] - a[i]);
+            a[i] = a[i-1];
+        }
+    
+    cout << ans << endl;
+}
